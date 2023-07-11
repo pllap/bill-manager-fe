7b7b99864cb01b36c26def4aa44fbc3f9d6a3ea3
Hyeokwoo Kwon <pllapallpal@gmail.com>
2023-07-12 03:39:36 +0900

    Add feature to change status of menuItem

--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -1,22 +1,54 @@
 import React, { useState } from 'react';
+import moment from 'moment';
 
 const MenuItem = ({ name }) => {
   // available status: waiting, on-going, cooked, delivered
   // TODO: click to send current status to server, and update status by server response
   const [status, setStatus] = useState('waiting');
+
+  let holdTimer = null;
+  let lastMouseDown = null;
+
   return (
-    <div key={name} className="card-text" onClick={handleClick}>
+    <div
+      key={name}
+      className="card-text"
+      onMouseDown={
+        () => {
+          lastMouseDown = moment().valueOf();
+          holdTimer = setTimeout(revertToPrevStatus, 500);
+        }
+      }
+      onMouseUp={
+        () => {
+          clearTimeout(holdTimer);
+          if (moment().valueOf() - lastMouseDown < 500) {
+            proceedToNextStatus();
+          }
+        }
+      }
+    >
       {name} ({status})
     </div>
   );
 
-  function handleClick() {
+  function proceedToNextStatus() {
     if (status === 'waiting') {
       setStatus('on-going');
     } else if (status === 'on-going') {
       setStatus('cooked');
     } else if (status === 'cooked') {
       setStatus('delivered');
+    }
+  }
+
+  function revertToPrevStatus() {
+    if (status === 'delivered') {
+      setStatus('cooked');
+    } else if (status === 'cooked') {
+      setStatus('on-going');
+    } else if (status === 'on-going') {
+      setStatus('waiting');
     }
   }
 }
